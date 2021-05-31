2201bb80ef59a283630f1cf9e4f29d0ac4bff473
SamKar <letmedevelopsam@gmail.com>
2021-06-01 00:16:11 +0300

    jd-ticketing-project-orm Task update added

--- src/main/java/com/cybertek/entity/Task.java
+++ src/main/java/com/cybertek/entity/Task.java
@@ -4,6 +4,7 @@ import com.cybertek.enums.Status;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Getter
 @Setter
+@Where(clause = "is_deleted=false")  //where is coming from HIBERNATE
 public class Task extends BaseEntity{
 
     private String taskSubject;
