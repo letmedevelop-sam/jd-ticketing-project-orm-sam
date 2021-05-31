bd48accbe584a0a8a9f768e817f022fb3d39f29f
SamKar <letmedevelopsam@gmail.com>
2021-05-31 11:07:33 +0300

    jd-ticketing-project-orm data sql added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into roles(description) values ('Admin');
+insert into roles(description) values ('Manager');
+insert into roles(description) values ('Employee');
+insert into roles(description) values ('Supervisor');
